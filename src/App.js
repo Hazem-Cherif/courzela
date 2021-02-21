11bbe8b0a60020d5664f647b623fdbe31ff7d503
Hazem <hazem.cherif@esprit.tn>
2021-02-21 22:53:29 +0100

    Coding Squad Pi Project 2021

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-
+import styled from "styled-components";
+import Header from "./Template/Header";
+import Footer from "./Template/Footer";
+import NavBar from "./Template/NavBar";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   <Header></Header>
+   <NavBar></NavBar>
+   <Footer></Footer>
+   </>
   );
 }
-
+const AppFrame = styled.div`
+text-align: center;
+display: flex;
+flex-direction: column;
+`;
 export default App;
---
+++ src/Template/Footer.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+export default class Footer extends Component {
+render() {
+return (
+<h2>byy</h2>
+);
+}
+}
